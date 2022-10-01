72997e9915a440f50df72fd0a11f63c1b4fafb4e
John Aeron Aragones <jaaragones@myseneca.ca>
2022-09-30 22:55:19 -0400

    stduents

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: John Aeron Aragones   Student ID: 121107213   Date: Sept 30 ,2022
+*
+*  Online (Cyclic) Link: https://awful-dog-shawl.cyclic.app
+*
+********************************************************************************/ 
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
 var path  = require('path');
+
 app.use(express.static('public'));
+var data = require(path.join(__dirname, 'data-service.js'));
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
@@ -12,6 +25,17 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
     res.sendFile(path.join(__dirname,'/views/about.html'));
 });
+
+app.get("/managers", (req, res) => {
+    data.getManagers()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((msg) => {
+        console.log("Error retrieving managers: " + msg);
+        res.json({ message: msg });
+      });
+  });
 
 // setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT);
