af3a7d2b53d584c70a07d7cb4b946fc03e5e6626
John Aeron Aragones <jaaragones@myseneca.ca>
2022-11-04 16:09:07 -0400

    SAVE part 1

--- server.js
+++ server.js
@@ -7,11 +7,11 @@
 *
 *  Online (Cyclic) Link: https://dead-red-shrimp-kit.cyclic.app
 *
-********************************************************************************/ 
+********************************************************************************/
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
-var path  = require('path');
+var path = require('path');
 
 app.use(express.static('public'));
 const dataServ = require('./data-service.js');
@@ -25,32 +25,57 @@ function onHttpStart() {
   console.log("Express http server listening on: " + HTTP_PORT);
 }
 
+// activeRoute
+app.use(function (req, res, next) {
+  let route = req.baseUrl + req.path;
+  app.locals.activeRoute = (route == "/") ? "/" : route.replace(/\/$/, "");
+  next();
+});
+
+
+
 // setting up Handlebars
-app.engine('.hbs', exphbs.engine({ extname: '.hbs',defaultLayout: "main" }));
+app.engine('.hbs', exphbs({
+  extname: '.hbs',
+  defaultLayout: "main",
+  helpers: {
+    navLink: function (url, options) {
+      return '<li' +
+        ((url == app.locals.activeRoute) ? ' class="active" ' : '') +
+        '><a href="' + url + '">' + options.fn(this) + '</a></li>';
+    },
+    equal: function (lvalue, rvalue, options) {
+      if (arguments.length < 3)
+        throw new Error("Handlebars Helper equal needs 2 parameters");
+      if (lvalue != rvalue) {
+        return options.inverse(this);
+      } else {
+        return options.fn(this);
+      }
+    }
+  }
+}));
 app.set('view engine', '.hbs');
-
-
-
 
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-// res.sendFile(path.join(__dirname,'/views/home.html'));
-res.render("home");
+  // res.sendFile(path.join(__dirname,'/views/home.html'));
+  res.render("home");
 });
 
 app.get("/about", (req, res) => {
-// res.sendFile(path.join(__dirname,'/views/about.html'));
-res.render("about");
+  // res.sendFile(path.join(__dirname,'/views/about.html'));
+  res.render("about");
 });
 
 app.get("/students/add", (req, res) => {
-// res.sendFile(path.join(__dirname,'/views/addStudent.html'));
-res.render("addStudent");
+  // res.sendFile(path.join(__dirname,'/views/addStudent.html'));
+  res.render("addStudent");
 });
 
 app.get("/images/add", (req, res) => {
-// res.sendFile(path.join(__dirname,'/views/addImage.html'));
+  // res.sendFile(path.join(__dirname,'/views/addImage.html'));
   res.render("addImage");
 });
 
@@ -63,19 +88,21 @@ app.get("/images", (req, res) => {
       obj.images.push(items[i]);
     }
     res.json(obj);
-  });});
+  });
+});
 
 
-  // Urlencoded 
-  app.use(bodyParser.urlencoded({ extended: true }));
+// Urlencoded 
+app.use(bodyParser.urlencoded({ extended: true }));
 
- // Adding multer
-  const storage = multer.diskStorage({
+// Adding multer
+const storage = multer.diskStorage({
   destination: "./public/images/uploaded",
   filename: function (req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname));
-  }});
-  const upload = multer({storage:storage});
+  }
+});
+const upload = multer({ storage: storage });
 
 
 app.engine('.hbs', exphbs.engine({ extname: '.hbs' }));
@@ -88,10 +115,11 @@ app.post("/images/add", upload.single("imageFile"), (req, res) => {
 });
 
 app.post("/students/add", (req, res) => {
-dataServ.addStudent(req.body)
-.then(() => {
-  res.redirect("/students");
-})});
+  dataServ.addStudent(req.body)
+    .then(() => {
+      res.redirect("/students");
+    })
+});
 
 
 // Retrieves Students Data
@@ -106,33 +134,33 @@ app.get("/students", (req, res) => {
       })
   }
   else if (req.query.program) {
-      dataServ.getStudentsByProgramCode(req.query.program)
-        .then((data) => {
-          res.json(data);
-        })
-        .catch((err) => {
-          res.send(err);
-        })
-    }
-    else if (req.query.expectedCredential) {
-      dataServ.getStudentsByExpectedCredential(req.query.expectedCredential)
-        .then((data) => {
-          res.json(data);
-        })
-        .catch((err) => {
-          res.send(err);
-        })
-    }
-    else {
-      dataServ.getAllStudents()
+    dataServ.getStudentsByProgramCode(req.query.program)
       .then((data) => {
-          res.json(data);
-        })
-        .catch((err) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.send(err);
+      })
+  }
+  else if (req.query.expectedCredential) {
+    dataServ.getStudentsByExpectedCredential(req.query.expectedCredential)
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.send(err);
+      })
+  }
+  else {
+    dataServ.getAllStudents()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
         console.log("Error retrieving students: " + err);
         res.json({ message: err });
-        });
-    }
+      });
+  }
   // dataServ.getAllStudents()
   //     .then((data) => {
   //       res.json(data);
@@ -141,46 +169,44 @@ app.get("/students", (req, res) => {
   //       console.log("Error retrieving students: " + err);
   //       res.json({ message: err });
   //     });
-  });
-  
-  app.get("/students/:studentID", (req, res) => {
-    dataServ.getStudentById(req.params.studentID)
-    .then((data) => {
-      res.json(data);
-  })
-  .catch((err) => {
-    console.log(err);
-    res.send(err);
-  })
 });
 
- 
-
-  app.get("/intlstudents", (req, res) => {
-      dataServ.getInternationalStudents()
-      .then((data) => {
-        res.json(data);
+app.get("/students/:studentID", (req, res) => {
+  dataServ.getStudentById(req.params.studentID)
+    .then((data) => {
+      res.json(data);
     })
-  });
+    .catch((err) => {
+      console.log(err);
+      res.send(err);
+    })
+});
 
-    app.get("/programs", (req, res) => {
-        dataServ.getPrograms()
-         .then((data) => {
-        res.json(data);
-      })
-  });
+app.get("/intlstudents", (req, res) => {
+  dataServ.getInternationalStudents()
+    .then((data) => {
+      res.json(data);
+    })
+});
+
+app.get("/programs", (req, res) => {
+  dataServ.getPrograms()
+    .then((data) => {
+      res.json(data);
+    })
+});
 
 
 
-  app.use((req, res) => {
-    res.status(404).send("<h2>404</h2><p>Im sorry! The page you are trying to reach is NOT FOUND!</p>");
+app.use((req, res) => {
+  res.status(404).send("<h2>404</h2><p>Im sorry! The page you are trying to reach is NOT FOUND!</p>");
 });
 
 
 // setup http server to listen on HTTP_PORT
 dataServ.initialize()
-.then(()=>{
+  .then(() => {
     app.listen(HTTP_PORT, onHttpStart);
-}).catch((err)=>{
+  }).catch((err) => {
     console.log("Error: ", err)
-})
+  })
